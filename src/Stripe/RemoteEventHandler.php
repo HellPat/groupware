b0da32b06103bdc2461882406d2b0a3821575c0a
Patrick Heller <patrick.heller@digistore24.team>
2024-01-09 10:28:41 +0100

    handles events

--- src/Stripe/RemoteEventHandler.php
+++ src/Stripe/RemoteEventHandler.php
@@ -2,13 +2,21 @@
 
 namespace App\Stripe;
 
+use Stripe\Event;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
 final class RemoteEventHandler
 {
+    /**
+     * @throws \JsonException
+     */
     public function __invoke(RemoteEvent $event): void
     {
-        // ...
+        $action = Event::constructFrom(
+            json_decode($event->payload, true, 512, JSON_THROW_ON_ERROR)
+        );
+        
+        // handle it by $action->type
     }
 }
