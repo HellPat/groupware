273b9e6d073958e0f76bf3570342e613ab721934
Patrick Heller <patrick.heller@digistore24.team>
2024-01-30 12:40:14 +0100

    updates deps

--- shell.nix
+++ shell.nix
@@ -3,7 +3,7 @@
     # https://nix.dev/tutorials/first-steps/towards-reproducibility-pinning-nixpkgs#pinning-packages-with-urls-inside-a-nix-expression
     # Picking the commit can be done via https://status.nixos.org,
     # which lists all the releases and the latest commit that has passed all tests.
-    pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/854f4671883250e456dc1553c783ac9741a0e9a4.tar.gz") {},
+    pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/c002c6aa977ad22c60398daaa9be52f2203d0006.tar.gz") {},
     php ? pkgs.php83.buildEnv {
       extensions = ({ enabled, all }: enabled ++ (with all; [
           redis
